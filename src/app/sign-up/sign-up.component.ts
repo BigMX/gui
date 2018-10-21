949021e2ea2a04de1dc3d6cfd27c0ddd572b7374
Arely Alcantara <arely0715@gmail.com>
2018-10-20 23:11:41 -0500

    Login page. Login page links to the signup page, attempted routing

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -12,6 +12,8 @@ export class SignUpComponent implements OnInit {
 
 alert: string;
 account: Account;
+color: string;
+state: number;
 constructor() { }
 
 ngOnInit() {
@@ -23,18 +25,23 @@ ngOnInit() {
     password: ''
   };
   this.alert = 'Not a valid email';
+
+  this.color: 'green';
+  this.state: 1;
 }
 
 onSearchChange(searchValue: string ) {
   if(validateEmail(searchValue)) {
     this.alert='valid email!!';
+    this.state=3;
   } else {
     this.alert='Not a valid email';
+    this.state=2;
   }
   console.log(validateEmail(searchValue));
 }
 
 function validateEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var re =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
