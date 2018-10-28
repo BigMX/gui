146ea2435418b2579b7b7664893ac3c6b3b28c0d
Arely Alcantara <arely0715@gmail.com>
2018-10-28 13:28:46 -0500

    user service file - attempted to set up endpoints for adding a user and getting a user by id

---
+++ src/app/class/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Account } from '../class/account';
+
+@Injectable()
+export class User {
+
+  protected endPoint = 'http://localhost:8080/api/users';
+
+  protected httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type' : 'application/json',
+    })
+  };
+
+  constructor(
+    protected httpClient: HttpClient
+  ) { }
+
+  add(account: Account): Observable<Account> {
+    return this.httpClient
+      .post<Account>(this.endPoint, account, this.httpOptions)
+      .pipe(catchError(this.handleException));
+  }
+
+  getById(id: number): Observable<Account> {
+    return this.httpClient
+      .get<Account>(`${this.endPoint}/${id}`, this.httpOptions)
+      .pipe(catchError(this.handleException));
+  }
+
+  protected handleException(exception: any) {
+    var message = `${exception.status} : ${exception.statusText}\r\n${exception.message}`;
+    alert(message);
+    return Observable.throw(exception);
+  }
+}
