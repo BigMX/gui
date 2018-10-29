0418857aaed1e47edee0a4f1bf1fa8fe0a199021
Hayaalhumaid <hkalhumaid@smu.edu>
2018-10-29 18:01:14 -0500

    Add files via upload

---
+++ sprint2/routes.php
@@ -0,0 +1,63 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+// Routes
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+
+//create account
+$app->get('/users', function ($request, $response, $args) { 
+	$sth = $this->dbConn->prepare(
+		"SELECT * FROM Users" );
+	$sth->execute();
+	$users = $sth->fetchAll();
+	return $this->response->withJson($users);
+});
+
+//login
+$app->get('/login', function ($request, $response, $args) { 
+	$sth = $this->dbConn->prepare(
+		"SELECT * FROM Login" );
+	$sth->execute();
+	$users = $sth->fetchAll();
+	return $this->response->withJson($users);
+});
+
+
+//registry
+$app->get('/registry', function ($request, $response, $args) { 
+	$sth = $this->dbConn->prepare(
+		"SELECT * FROM Registries" );
+	$sth->execute();
+	$users = $sth->fetchAll();
+	return $this->response->withJson($users);
+});
+
+
+//item
+$app->get('/items', function ($request, $response, $args) { 
+	$sth = $this->dbConn->prepare(
+		"SELECT * FROM Items" );
+	$sth->execute();
+	$users = $sth->fetchAll();
+	return $this->response->withJson($users);
+});
+
+//shopping cart
+$app->get('/cart', function ($request, $response, $args) { 
+	$sth = $this->dbConn->prepare(
+		"SELECT * FROM Cart" );
+	$sth->execute();
+	$users = $sth->fetchAll();
+	return $this->response->withJson($users);
+});
