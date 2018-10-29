5426071519a35c376c7cccf529eaefb0c47a6b2b
BigMX <smei@smu.edu>
2018-10-29 18:18:10 -0500

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
-
+import { ModalDialogModule } from 'ngx-modal-dialog';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-
+import { ModalModule, BsModalRef } from 'ngx-bootstrap';
 import { LogInComponent } from './log-in/log-in.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ArchiveComponent } from './archive/archive.component';
 import { InviteComponent } from './invite/invite.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 const defaultRoute = 'login';
 
@@ -21,12 +21,14 @@ const defaultRoute = 'login';
     SignUpComponent,
     DashboardComponent,
     ArchiveComponent,
-    InviteComponent
-    LogInComponent
+    InviteComponent,
+    LogInComponent,
   ],
   imports: [
+    ModalModule,
     BrowserModule,
     FormsModule,
+    ModalDialogModule.forRoot(),
     RouterModule.forRoot([
       {path: 'dashboard', component: DashboardComponent},
       {path: 'invite', component: InviteComponent},
@@ -38,7 +40,7 @@ const defaultRoute = 'login';
     ])
   ],
   exports: [RouterModule],
-  providers: [],
+  providers: [BsModalRef],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -25,22 +25,19 @@ ngOnInit() {
     password: ''
   };
   this.alert = 'Not a valid email';
-  this.color: 'green';
-  this.state: 1;
+  // this.color: 'green';
+  // this.state: 1;
 }
 
 onSearchChange(searchValue: string ) {
-  if(validateEmail(searchValue)) {
-    this.alert='valid email!!';
-    this.state=3;
-  } else {
-    this.alert='Not a valid email';
-    this.state=2;
-  }
-  console.log(validateEmail(searchValue));
+  // if(validateEmail(searchValue)) {
+  //   this.alert='valid email!!';
+  //   this.state=3;
+  // } else {
+  //   this.alert='Not a valid email';
+  //   this.state=2;
+  // }
+  // console.log(validateEmail(searchValue));
+  return 0;
 }
-
-function validateEmail(email) {
-  var re =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
 }
