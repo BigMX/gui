02251cbff12cc7744560585c133eb41a6edcad92
Hayaalhumaid <hkalhumaid@smu.edu>
2018-10-29 18:32:45 -0500

    Update routes.php

--- sprint2/routes.php
+++ sprint2/routes.php
@@ -4,13 +4,13 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 // Routes
 
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-});
+// $app->add(function ($req, $res, $next) {
+//     $response = $next($req, $res);
+//     return $response
+//             ->withHeader('Access-Control-Allow-Origin', '*')
+//             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+//             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+// });
 
 
 //create account
