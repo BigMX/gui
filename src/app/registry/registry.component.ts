76a9c52f8304ae67daf3c4ef7cac64fd32f3f6c3
Arely Alcantara <arely0715@gmail.com>
2018-11-04 23:12:15 -0600

    fixing redirecting to registry pages view

--- src/app/registry/registry.component.ts
+++ src/app/registry/registry.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Registry } from '../class/registry';
+import { Registries } from '../class/registries.service';
+
+class RegistryParams {
+  id: string;
+}
 
 @Component({
   selector: 'app-registry',
@@ -9,18 +14,39 @@ import { Registry } from '../class/registry';
 })
 export class RegistryComponent implements OnInit {
 
+  currentReg: Registry;
   registry: Registry[];
   newRegistry: Registry;
+  id = 2;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private registries: Registries
+  ) { }
 
   ngOnInit() {
     this.newRegistry = new Registry;
-    this.registry = [];
+    this.registries.getRegistries(this.id).subscribe((registry) => {
+      this.registry = registry;
+    });
+    this.route.params.subscribe((params: RegistryParams) => {
+      if (params.id) {
+        this.registries.getRegById(+params.id).subscribe((registry) => {
+          this.currentReg = registry;
+        });
+      }
+    });
   }
 
   addRegistry() {
-    this.registry.push(this.newRegistry);
-    this.newRegistry = new Registry;
+    this.newRegistry.userId = this.id;
+    this.newRegistry.status = 'active';
+    this.registries.add(this.newRegistry).subscribe((registry) => {
+    });
+    this.registries.getRegistries(this.id).subscribe((registry) => {
+      this.registry = registry;
+    });
+    this.newRegistry = {};
   }
+
 }
