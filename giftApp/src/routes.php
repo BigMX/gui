2b0d41acd5aa382e7a1c93952729877f1d8bf5de
Hayaalhumaid <hkalhumaid@smu.edu>
2018-11-08 19:34:33 -0600

    Update routes.php

--- giftApp/src/routes.php
+++ giftApp/src/routes.php
@@ -3,12 +3,44 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-// Routes
-
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+//Routes
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
 });
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+
+$user_id=-1;
+
+// adding a new user
+$app->post('/addusers', function ($request, $response) {
+    $input = $request->getParsedBody();
+    $sql = "INSERT INTO 
+        Users (firstName,lastName,phone,email, password_) 
+        VALUES (:firstName,:lastName,:phone,:email, :password_)";
+    $sth = $this->dbConn->prepare($sql);
+    $sth->bindParam("firstName", $input['firstName']);
+    $sth->bindParam("lastName", $input['lastName']);
+    $sth->bindParam("phone", $input['phone']);
+    $sth->bindParam("email", $input['email']);
+    $sth->bindParam("password_", $input['password_']);
+    $sth->execute();
+    return $this->response->withJson($input);
+});
+// 
+//create account (displays all the users inserted)
+$app->get('/users', function ($request, $response, $args) { 
+	$sth = $this->dbConn->prepare(
+		"SELECT * FROM Users" );
+	$sth->execute();
+	$users = $sth->fetchAll();
+	return $this->response->withJson($users);
+});
+
+ 
