29444203c7a6a1200bc0f8b11d1f569b90d3b458
Arely Alcantara <arely0715@gmail.com>
2018-11-09 15:56:35 -0600

    adde sidebar component and fixing things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ArchiveComponent } from './archive/archive.component';
 import { InviteComponent } from './invite/invite.component';
 import { RegistryComponent } from './registry/registry.component';
 import { CartComponent } from './cart/cart.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 // services
 import { Registries } from './class/registries.service';
@@ -30,7 +31,8 @@ const defaultRoute = 'login';
     InviteComponent,
     LogInComponent,
     RegistryComponent,
-    CartComponent
+    CartComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Registry } from '../class/registry';
+import { Registries } from '../class/registries.service';
+import { Account } from '../class/account';
+import { User } from '../class/user.service';
+
+class SidebarParams {
+  id: string;
+}
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  account: Account;
+  id: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private registries: Registries,
+    private users: User,
+    protected router: Router
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: SidebarParams) => {
+      if (params.id) {
+        this.id = +params.id;
+        this.users.getById(this.id).subscribe((acct) => {
+          this.account = acct;
+        });
+      }
+    });
+  }
+
+}
