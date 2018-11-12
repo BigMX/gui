1cf24ad82272681ac1d77341efa9739edad91dd0
Arely Alcantara <arely0715@gmail.com>
2018-11-12 01:48:57 -0600

    notifications working to some extent

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,7 +1,9 @@
+import { Registries } from './../class/registries.service';
 import { Account } from './../class/account';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from '../class/user.service';
+import { Registry } from '../class/registry';
 
 class NotificationsParams {
   id: string;
@@ -16,20 +18,28 @@ export class NotificationsComponent implements OnInit {
 
   id: number;
   account: Account;
-  notifs: string[];
+  registryList: Registry[] = [];
+  notifs: string[] = [];
 
   constructor(
     private route: ActivatedRoute,
-    private users: User
+    private users: User,
+    private registries: Registries
   ) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: NotificationsParams) => {
       if (params.id) {
         this.id = +params.id;
+        this.registries.getRegistries(this.id).subscribe((registries) => {
+          this.registryList = registries;
+        });
         this.users.getById(this.id).subscribe((account) => {
+          if (account.notifications !== undefined) {
           this.notifs = account.notifications;
+          }
           this.account = account;
+          this.check();
         });
       }
     });
@@ -41,6 +51,38 @@ export class NotificationsComponent implements OnInit {
     this.account.notifications = this.notifs;
     this.users.removeNotif(this.account).subscribe((account) => {
       // console.log(account);
+    });
+  }
+
+  check() {
+    let i;
+    // tslint:disable-next-line:forin
+    for(i = 0; i < this.registryList.length; i++) {
+      const registry = this.registryList[i];
+      console.log(registry);
+      if (registry.items === undefined) {
+        const message = 'all items have been bought for ' + registry.name + '!!!!';
+        if(this.notifs.indexOf(message)===-1) {
+          this.notifs.push(message);
+        }
+      }
+       let j;
+       if(registry.items!==undefined) {
+        for (j = 0; j < registry.items.length; j++) {
+            const item = registry.items[j];
+            if (item.status === 'bought') {
+              const message = item.name + ' has been bought!!!';
+              if(this.notifs.indexOf(message)===-1) {
+                this.notifs.push(message);
+              }
+            }
+          }
+       }
+    }
+    console.log(this.notifs);
+    this.account.notifications = this.notifs;
+    this.users.addNotif(this.account).subscribe((account) => {
+
     });
   }
 
