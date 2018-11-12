b0d226bd789acc664f059bfa146eadf36f457d01
Arely Alcantara <arely0715@gmail.com>
2018-11-12 00:43:49 -0600

    adding an item to cart works - created a modal for adding an item

--- src/app/class/user.service.ts
+++ src/app/class/user.service.ts
@@ -44,6 +44,12 @@ export class User {
     .pipe(catchError(this.handleException));
   }
 
+  addItemToCart(user: Account): Observable<Account> {
+    return this.httpClient
+    .put<Account>(`${this.endPoint}/${user.id}`, user, this.httpOptions)
+    .pipe(catchError(this.handleException));
+  }
+
   protected handleException(exception: any) {
     const message = `${exception.status} : ${exception.statusText}\r\n${exception.message}`;
     alert(message);
