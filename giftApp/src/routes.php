59407fae141ef8e1e2c19fd9ac650ab396c2ab2d
hayaalhumaid <hkalhumaid@smu.edu>
2018-11-25 17:55:13 -0600

    .

--- giftApp/src/routes.php
+++ giftApp/src/routes.php
@@ -256,13 +256,14 @@ $app->get('/getboughtitem/{user_id}', function ($request, $response, $args) {
 //gets all the registries assositated with the user
 $app->put('/getregistries', function ($request, $response, $args) {
     $input = $request->getParsedBody();
-    $sql = "SELECT u.user_id, u.lastname,u.firstname,i.name, i.item_id FROM Registries r INNER JOIN Items i ON r.registry_id=i.registry_id INNER JOIN Users u On u.user_id=i.user_id";
+    $sql = "SELECT u.user_id, u.lastname,u.firstname,i.name, i.item_id FROM Registries r INNER JOIN Items i ON r.registry_id=i.registry_id INNER JOIN Users u On u.user_id=i.user_id WHERE registry_id=:registry_id";
     $sth = $this->dbConn->prepare($sql);
-    //$sth->bindParam("staus", $input['status']);
+    $sth->bindParam("registry_id", $input['registry_id']);
     $res = $sth->execute();
     $user = $sth->fetchAll();
     return $this->response->withJson($user);
 });
+
 //
 //get item name
 $app->get('/getitemname/{name}', function ($request, $response, $args) { 
